2ba589f0157354e21d5de0bd7555105aa1901985
Curryboy knows how to code <sanju2506g@gmail.com>
2023-12-09 01:12:40 +0000

    refactored corner detection

--- Image_processing_module/corner_detection.py
+++ Image_processing_module/corner_detection.py
@@ -15,13 +15,7 @@ class ChessboardCornerDetection:
         self.pixel_coordinates = None # the coordinate of each corner in each square in the original image
         
       
-       
-    
-
-    def _pre_process_img(self):
-        
-    
-
+    def __pre_process_img(self):
         THRESHOLD = 100
         # Convert to gray scale image
         self.processing_img = cv2.cvtColor(self.processing_img, cv2.COLOR_BGR2GRAY)
@@ -32,7 +26,7 @@ class ChessboardCornerDetection:
         ret, self.processing_img = cv2.threshold(self.processing_img, THRESHOLD, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
 
-    def locate_chessboard_and_get_4_external_corners(self):
+    def __locate_chessboard_and_get_4_external_corners(self):
         # find the all the contours of the image,ie the white parts of the binary image
         contours, _ = cv2.findContours(self.processing_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         # Find the largest  enclosed contour area which should be the chessboard
@@ -58,43 +52,33 @@ class ChessboardCornerDetection:
 
                 #     cv2.circle(self.drawing_original_img, tuple(corner), 5, (0, 0, 255), -1)
                 #     cv2.putText(self.drawing_original_img, str(i), tuple(corner), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
                 return corners
         except IndexError:
             pass
 
 
-    def crop_and_get_top_down_view_of_chessboard(self, corners_for_perspective_transform):
+    def __crop_and_get_top_down_view_of_chessboard(self, corners_for_perspective_transform):
         # the required width and height of the warped image
         WIDTH = 500
         HEIGHT = 500
-        
-        # crop the left and right sides of the image
-        
-       
         # Assume `crop_amount` is the amount you want to crop from both sides
-        crop_amount_sides = -20  # Change this to the amount you want to crop
+        CROP_AMOUNT_SIDES = -20  # Change this to the amount you want to crop
         
-        crop_amount_top = -10
+        CROP_AMOUNT_TOP = -10
         # Adjust the destination points
-        destination_points = destination_points = np.array([[WIDTH - crop_amount_sides, crop_amount_top], [WIDTH - crop_amount_sides, HEIGHT], [crop_amount_sides, HEIGHT],[crop_amount_sides, crop_amount_top]], dtype='float32')
-
+        destination_points = destination_points = np.array([[WIDTH - CROP_AMOUNT_SIDES, CROP_AMOUNT_TOP], [WIDTH - CROP_AMOUNT_SIDES, HEIGHT], [CROP_AMOUNT_SIDES, HEIGHT],[CROP_AMOUNT_SIDES, CROP_AMOUNT_TOP]], dtype='float32')
         # get the transformation matrix for a top down view of the original image
         self.transformation_matrix = cv2.getPerspectiveTransform(corners_for_perspective_transform, destination_points)
         self.inverse_tranformation_matrix = cv2.getPerspectiveTransform(destination_points, corners_for_perspective_transform)
-        
         # apply the transformation matrix to the cropped image
         self.warped_img = cv2.warpPerspective(self.original_img, self.transformation_matrix, (WIDTH, HEIGHT))
-        
-
         # show the top down view of the chessboard
         # cv2.imshow('Warped image', self.warped_img)
 
     
   
-    def get_pixel_coordinates_using_shi_tomasi_on_warped_img(self):
-    
-        img_copy = self.warped_img.copy()
+    def __get_pixel_coordinates_using_shi_tomasi_on_warped_img(self):
+        #img_copy = self.warped_img.copy()
         # convert warped image to grayscale
         self.warped_img = cv2.cvtColor(self.warped_img, cv2.COLOR_BGR2GRAY)
         # apply a strong gaussian blur to prevent numbers and letters of the chessboard from being detected as corners
@@ -122,7 +106,6 @@ class ChessboardCornerDetection:
                 for i in range(0, len(corners), 9):  # Change 9 to the number of corners per row
                     # Sort the row based on the x-coordinate and append it to the sorted corners
                     sorted_corners.append(sorted(corners[i:i+9], key=lambda x: x[0]))
-
                 corners = np.reshape(sorted_corners, (81, 1, 2))
                 # for i,corner in enumerate(corners):
                 #     cv2.circle(img_copy, (int(corner[0][0]), int(corner[0][1])), 2, (0, 0, 255), -1)
@@ -130,14 +113,12 @@ class ChessboardCornerDetection:
 
                 # # show the warped image with the corners
                 # cv2.imshow('deteted corners on warped image', img_copy)
-
                 # convert warped plane corners to original plane corners using the inverse transformation matrix we got from the reverse perspective transform
                 self.pixel_coordinates = np.dot(self.inverse_tranformation_matrix, np.array([corners[:, 0, 0], corners[:, 0, 1], np.ones(corners.shape[0])]))
                 self.pixel_coordinates = self.pixel_coordinates[:2,:] / self.pixel_coordinates[2, :]
                 self.pixel_coordinates = np.transpose(self.pixel_coordinates)
                 corners = np.float32(self.pixel_coordinates)
                 self.pixel_coordinates = np.reshape(self.pixel_coordinates, (81, 1, 2))
-
                 #draw the corners on the original image and add text of the current index to the
                 # for i in range(len(corners)):
                 #     cv2.circle(self.drawing_original_img, (int(corners[i][0]), int(corners[i][1])), 2, (0, 0, 255), -1)
@@ -145,45 +126,38 @@ class ChessboardCornerDetection:
                 #     cv2.putText(self.drawing_original_img, str(i), (int(corners[i][0]), int(corners[i][1])), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
                 # draw chessboard corners with drawchessboarcorners
-                cv2.drawChessboardCorners(self.drawing_original_img, (9, 9), corners, True)
+                #cv2.drawChessboardCorners(self.drawing_original_img, (9, 9), corners, True)
                 # show the original image with the corners
-                
-        
         except ValueError:
             print("No corners found")
-
-
-    
 
 
 
 
     def recognise_corners(self, frame):
-
-        # use our class as process the images
+        # use our class to process the images and get corners
         # set the original image and processing image to the frame
         self.original_img = frame
         self.drawing_original_img = frame
         self.processing_img = frame
         # pre process the image for getting the chessboard
-        self._pre_process_img()
+        self.__pre_process_img()
         # locate the chessboard by finding largest contour
-        corners_for_perspective_transform = self.locate_chessboard_and_get_4_external_corners()
+        corners_for_perspective_transform = self.__locate_chessboard_and_get_4_external_corners()
 
         if corners_for_perspective_transform is not None:
             #crop and transform the image to a top down view, passing in false so that we dont crop the image
-            self.crop_and_get_top_down_view_of_chessboard(corners_for_perspective_transform)
-
+            self.__crop_and_get_top_down_view_of_chessboard(corners_for_perspective_transform)
             #get the pixel coordinates of each corner of each square on the chessboard, passing in false so that we draw the corners
-            self.get_pixel_coordinates_using_shi_tomasi_on_warped_img()
-            cv2.imshow('Warped image', self.warped_img)
-            self.convert_pixel_coordinates_to_3d()
+            self.__get_pixel_coordinates_using_shi_tomasi_on_warped_img()
+            #cv2.imshow('Warped image', self.warped_img)
+            
 
 
         # show the images
-        cv2.imshow('original image', self.original_img)
-        cv2.imshow('drawing original image', self.drawing_original_img)
-        cv2.imshow('Processing image', self.processing_img)
+        # cv2.imshow('original image', self.original_img)
+        # cv2.imshow('drawing original image', self.drawing_original_img)
+        # cv2.imshow('Processing image', self.processing_img)
     
 
 
